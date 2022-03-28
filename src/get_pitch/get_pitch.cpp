d260c34e18adf68d08c4a525f271ad72914626cb
aaaaluki <luki00.lrb@gmail.com>
2022-03-28 10:15:14 +0200

    Segunda clase lab

--- src/get_pitch/get_pitch.cpp
+++ src/get_pitch/get_pitch.cpp
@@ -25,6 +25,7 @@ Usage:
     get_pitch --version
 
 Options:
+    -m FLOAT, --umaxnorm FLOAT  Umbral del máximo de la autocorrelación normalizada [default: 0.45]
     -h, --help  Show this screen
     --version   Show the version of the project
 
@@ -46,6 +47,7 @@ int main(int argc, const char *argv[]) {
 
 	std::string input_wav = args["<input-wav>"].asString();
 	std::string output_txt = args["<output-txt>"].asString();
+    float umaxnorm = stof(args["--umaxnorm"].asString());
 
   // Read input sound file
   unsigned int rate;
@@ -59,7 +61,7 @@ int main(int argc, const char *argv[]) {
   int n_shift = rate * FRAME_SHIFT;
 
   // Define analyzer
-  PitchAnalyzer analyzer(n_len, rate, PitchAnalyzer::RECT, 50, 500);
+  PitchAnalyzer analyzer(n_len, rate, umaxnorm, PitchAnalyzer::RECT, 50, 500);
 
   /// \TODO
   /// Preprocess the input signal in order to ease pitch estimation. For instance,
--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -57,7 +57,7 @@ namespace upc {
     /// \TODO Implement a rule to decide whether the sound is voiced or not.
     /// * You can use the standard features (pot, r1norm, rmaxnorm),
     ///   or compute and use other ones.
-    return false;
+    return rmaxnorm < m_umaxnorm;
   }
 
   float PitchAnalyzer::compute_pitch(vector<float> & x) const {
