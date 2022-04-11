4336117b745b77365359e0753ef3f2c113f316e4
aaaaluki <luki00.lrb@gmail.com>
2022-04-11 14:18:06 +0200

    Ahora a run_get_pitch.sh se le pueden pasar argumentos

--- scripts/run_get_pitch.sh
+++ scripts/run_get_pitch.sh
@@ -6,7 +6,7 @@ GETF0="get_pitch"
 for fwav in pitch_db/train/*.wav; do
     ff0=${fwav/.wav/.f0}
     echo "$GETF0 $fwav $ff0 ----"
-    $GETF0 $fwav $ff0 > /dev/null || (echo "Error in $GETF0 $fwav $ff0"; exit 1)
+    $GETF0 $@ $fwav $ff0 > /dev/null || (echo -e "Error in $GETF0 $fwav $ff0\n"; exit 1)
 done
 
 exit 0
